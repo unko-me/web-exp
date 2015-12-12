00bfbcc10bae50c280704f5faa39d1de14ad29cf
katapad <katapad@gmail.com>
2015-12-13 04:31:16 +0900

    サンプルシェーダー読み込みおk

--- src/typescript/pages/shader/SimpleShader.main.ts
+++ src/typescript/pages/shader/SimpleShader.main.ts
@@ -3,6 +3,13 @@ import Material = THREE.Material;
 import {PageDetailUtil} from "../../core/PageDetailUtil";
 
 export class SimpleShader extends BaseWorld {
+  private uniformsPudding:any;
+
+  private angle:number = 0;
+  private swingVec:THREE.Vector3;
+  private swingStrength:number = 0.0;
+
+
   constructor() {
     super();
     PageDetailUtil.setPageData({title: "HTMLすら作らないタイプのページ"})
@@ -11,11 +18,43 @@ export class SimpleShader extends BaseWorld {
     //this._setupGround();
 
     var frag:string = <string>require('../../../shader/purupuru/1.frag');
-    var vert:string = <string>require('../../../shader/purupuru/1.frag');
+    var vert:string = <string>require('../../../shader/purupuru/1.vert');
+    this.swingVec = new THREE.Vector3(0, 1, 0);
+    var swingStrength = 0;
+    this.uniformsPudding = {
+      frame:{
+        type: 'f',// float型
+        value: 0.0
+      },
+      modelHeight:{
+        type:"f", // float型
+        value: 5
+      },
+      swingVec: {
+        type:"v3", // Vector3型
+        value: this.swingVec
+      },
+      swingStrength: {
+        type:"f", // float型
+        //value: this.swingStrength
+        value: swingStrength
+      }
+    };
+
+    this.uniformsPudding = THREE.UniformsUtils.merge([THREE.ShaderLib.phong.uniforms, this.uniformsPudding]);
+
+    var shaderMaterialPudding = new THREE.ShaderMaterial({
+      uniforms: this.uniformsPudding,
+      // シェーダーを割り当てる
+      vertexShader: vert,
+      //fragmentShader: THREE.ShaderLib.phong.fragmentShader
+      fragmentShader: frag
+    });
+
 
     const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material:Material = new THREE.MeshPhongMaterial({color: 0xdd3b6f});
-    const cube = new THREE.Mesh(geometry, material);
+    //const material:Material = new THREE.MeshPhongMaterial({color: 0xdd3b6f});
+    const cube = new THREE.Mesh(geometry, shaderMaterialPudding);
     this.scene.add(cube);
 
   }
@@ -31,7 +70,15 @@ export class SimpleShader extends BaseWorld {
 
     mesh.rotation.x = -90 * Math.PI / 180;
   }
+  protected _update():void {
+    this.angle += 0.01;
+    this.swingVec.x = Math.sin(this.angle) * 5.5 + 0.5;
+    this.swingVec.y = Math.sin(this.angle) * 0.5 + 0.5;
+    this.uniformsPudding.swingVec.value = this.swingVec;
+    this.uniformsPudding.swingStrength.value = Math.cos(this.angle) * 2 + 2;
 
+
+  }
 }
 
 

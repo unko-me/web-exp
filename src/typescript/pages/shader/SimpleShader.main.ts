e9e123124ad47d001801e19b4fd823115fb03329
katapad <katapad@gmail.com>
2015-12-14 09:36:15 +0900

    復数とばす

--- src/typescript/pages/shader/SimpleShader.main.ts
+++ src/typescript/pages/shader/SimpleShader.main.ts
@@ -13,6 +13,7 @@ export class SimpleShader extends BaseWorld {
   private texture:Texture;
   private gui:GUI;
   private money:Money;
+  private moneys:Array<Money>;
 
 
   constructor() {
@@ -27,35 +28,57 @@ export class SimpleShader extends BaseWorld {
     var loader = new THREE.TextureLoader();
     loader.load('/img/sozai/1000000-512.jpg', (texture)=> {
       this.texture = texture;
-      this._createMoney();
+      this._createMoneys(texture);
     });
 
   }
 
-  private _createMoney():void {
-    this.params = Money.DEF_VALUE;
-
-    this.gui = new GUI();
-    this.gui.add(this.params, 'angleV', 0.01, 2.0);
-    this.gui.add(this.params, 'amp', .1, 100);
-    this.gui.add(this.params, 'freq', 0.1, 100);
-    this.gui.add(this.params, 'factor', -5, 5);
-    this.gui.add(this.params, 'factorDecay', -2, 2);
-
-    this.money = new Money(this.texture, this.params);
-    this.scene.add(this.money.mesh);
-  };
+  //private _createMoney():void {
+  //  this.params = Money.DEF_VALUE;
+  //
+  //  this.gui = new GUI();
+  //  this.gui.add(this.params, 'angleV', 0.01, 2.0);
+  //  this.gui.add(this.params, 'amp', .1, 100);
+  //  this.gui.add(this.params, 'freq', 0.1, 100);
+  //  this.gui.add(this.params, 'factor', -5, 5);
+  //  this.gui.add(this.params, 'factorDecay', -2, 2);
+  //
+  //  this.money = new Money(this.texture, this.params);
+  //  this.scene.add(this.money.mesh);
+  //};
 
   protected _update():void {
-    if (!this.money) return;
-    this.money.update();
+    if (!this.moneys) return;
+    for (let money of this.moneys) {
+      money.update();
+    }
+  }
+  //protected _update():void {
+  //  if (!this.money) return;
+  //  this.money.update();
+  //}
+
+  private _createMoneys(texture:THREE.Texture):void {
+    this.moneys = [];
+    for(let i = 0; i < 10; i++) {
+      let params = _.cloneDeep(Money.DEF_VALUE);
+      params.freq += Math.random()* 2 - 1;
+      params.angleV += (Math.random() - 0.5) * 0.1;
+      params.amp += (Math.random() - 0.5) * 4;
+      params.factor += (Math.random() - 0.5) * 1;
+
+
+      var money = new Money(texture, params);
+      this.moneys.push(money);
+      this.scene.add(money.mesh);
+      money.mesh.position.x = (Math.random() - 0.5) * 1000;
+      money.mesh.position.y = (Math.random() - 0.5) * 1000;
+      money.mesh.position.z = (Math.random() - 0.5) * 1000;
+    }
   }
 }
 
 
 new SimpleShader().startLoop();
-
-
-
 
 
